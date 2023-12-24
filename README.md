4578086391879a0d24ded718dce79903c0e95e29
igorplyukhin <51264207+igorplyukhin@users.noreply.github.com>
2023-12-24 22:49:38 +0500

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+### todoList demo springBoot project
+
+Task - задача из todoList
+* id
+* name
+* description
+* category
+* isComplete
+
+Category - сущность для группировки задач по категориям 
+* id
+* name
+
+User - сущность пользователя
+* username
+* password
---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.1"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.hibernate.orm:hibernate-community-dialects")
+	implementation("org.xerial:sqlite-jdbc:3.40.1.0")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-devtools")
+	implementation("org.projectlombok:lombok")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
